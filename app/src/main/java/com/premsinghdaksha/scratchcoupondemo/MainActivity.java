5fffe2d41830c23c894f86e6bc6019519dbd8095
Er. prem singh daksha <46933759+premsingh8171@users.noreply.github.com>
2021-02-16 16:27:12 +0530

    update code

---
+++ app/src/main/java/com/premsinghdaksha/scratchcoupondemo/MainActivity.java
@@ -0,0 +1,30 @@
+package com.premsinghdaksha.scratchcoupondemo;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.premsinghdaksha.scratchcoupon.ScratchCardView;
+
+public class MainActivity extends AppCompatActivity {
+    ScratchCardView scratchCardView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        scratchCardView = findViewById(R.id.scratchCardView);
+        scratchCardView.setOnScratchListener(new ScratchCardView.OnScratchListener() {
+            @Override
+            public void onScratch(ScratchCardView scratchCard, float visiblePercent) {
+                if (visiblePercent > 0.7) {
+                    scratchCard.setVisibility(View.GONE);
+                    Toast.makeText(MainActivity.this, "YOU EARNED 20", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+}
